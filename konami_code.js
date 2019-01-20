53e3e9f24d80a3102064476054c08fcae29f00f4
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-20 21:55:51 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init() {
   document.body.addEventListener("keydown", function(e) {
     const key = e.key
     
-    if(codes[index] === key){
+    if(alphabet[index] === key){
      index++;
     }
     if (index === codes.length) {
